7d94c131310cc8387b373af51365e6201fd60baf
Norbert <norbert.lipinski@forecast.global>
2022-07-11 12:49:05 +0200

    Added source

--- agent_norbert.py
+++ agent_norbert.py
@@ -5,21 +5,23 @@ import pandas as pd
 import datetime as dt
 
 CHANCE_TO_HAVE = 0.6
+SOURCES = ['display_ads', 'video_ads', 'svod']
 
 # 1,5edf63c9-a4de-4592-b70d-8fa137c00f88,2019-02-01,2.158469060601665
 
 
 class User:
-    def __init__(self, account_id: str, date_key: str, revenue: float) -> None:
+    def __init__(self, account_id: str, date_key: str, revenue: float, source: str) -> None:
 
-        self._validate_input(account_id, date_key, revenue)
+        self._validate_input(account_id, date_key, revenue, source)
 
         self.account_id = account_id
         self.date_key = date_key
         self.revenue = revenue
+        self.source = source
 
     @staticmethod
-    def _validate_input(account_id: str, date_key: str, revenue: float) -> None:
+    def _validate_input(account_id: str, date_key: str, revenue: float, source: str) -> None:
         if not isinstance(account_id, str):
             raise ValueError(
                 f"Wrong type for {account_id} - expected string, got {type(account_id)}!"
@@ -34,9 +36,13 @@ class User:
             raise ValueError(
                 f"Wrong type for {revenue} - expected float, got {type(revenue)}!"
             )
+        if not isinstance(source, str):
+            raise ValueError(
+                f"Wrong type for {source} - expected float, got {type(source)}!"
+            )
 
-    def get_info(self) -> List[Union[str, str, float]]:
-        return [self.account_id, self.date_key, self.revenue]
+    def get_info(self) -> List[Union[str, str, float, str]]:
+        return [self.account_id, self.date_key, self.revenue, self.source]
 
 
 class RecordsCreator:
@@ -59,34 +65,35 @@ class RecordsCreator:
         for idx, _ in enumerate(range(self.no_accounts)):
             account_id = str(uuid.uuid4())
 
-            for date in date_range:
-                revenue = float(random.random() * 10)
+            for src in SOURCES:
+                source = src
 
-                if idx == 0:
-                    user = User(account_id, date, revenue)
-                    self.users.append(user)
-                    self.output.append([account_id, date, revenue])
-                else:
-                    if self.coin_flip(random.random()):
-                        user = User(account_id, date, revenue)
+                for date in date_range:
+                    revenue = float(random.random() * 10)
+
+                    if idx == 0:
+                        user = User(account_id, date, revenue, source)
                         self.users.append(user)
-                        self.output.append([account_id, date, revenue])
+                        self.output.append([account_id, date, revenue, source])
+                    else:
+                        if self.coin_flip(random.random()):
+                            user = User(account_id, date, revenue, source)
+                            self.users.append(user)
+                            self.output.append([account_id, date, revenue, source])
 
     def coin_flip(self, guess: float) -> bool:
         return guess < CHANCE_TO_HAVE
 
     def generate(self) -> None:
         self.df = pd.DataFrame(
-            self.output, columns=["account_id", "date_key", "revenue"]
+            self.output, columns=["account_id", "date_key", "revenue", "source"]
         )
         self.df.to_csv(path_or_buf=f"./output_norbert.csv", index=True)
 
 
 if __name__ == "__main__":
 
-    users_records = RecordsCreator(
-        no_accounts=1000, start_date="2019-01-01", end_date="2022-05-01"
-    )
+    users_records = RecordsCreator(no_accounts=1000, start_date="2019-01-01", end_date="2020-12-01")
     users_records.fullfill()
     users_records.generate()
     print("Done.")
